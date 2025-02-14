080a43d15f23a7092ca82cdc857a46bad277d350
Sayazrahman <115418700+Sayazrahman@users.noreply.github.com>
2025-02-14 15:15:05 +0530

    setup deployment

--- src/component/dashboard/dashboard.tsx
+++ src/component/dashboard/dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { FaEdit, FaTrash, FaPlus } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { toast, ToastContainer } from "react-toastify";
